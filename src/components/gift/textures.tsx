b10d0a78d9e1361e05636208ccebc52dfc2e7386
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-17 17:36:07 -0500

    Add svg textures for gift sides

---
+++ src/components/gift/textures.tsx
@@ -0,0 +1,353 @@
+export function Top() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="100%"
+      height="100%"
+      viewBox="0 0 67.733 67.733"
+    >
+      <defs>
+        <pattern
+          xlinkHref="#b"
+          id="a"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="b"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <pattern
+          id="c"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <pattern
+          xlinkHref="#e"
+          id="k"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          xlinkHref="#e"
+          id="l"
+          patternTransform="rotate(45 81.758 -33.866)scale(4.4039)"
+        ></pattern>
+        <pattern
+          xlinkHref="#f"
+          id="e"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="f"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <linearGradient id="g">
+          <stop offset="0" stopColor="#970000"></stop>
+          <stop offset="1" stopColor="#f55"></stop>
+        </linearGradient>
+        <linearGradient id="d">
+          <stop offset="0" stopColor="#f55"></stop>
+          <stop offset="1" stopColor="#970000"></stop>
+        </linearGradient>
+        <radialGradient
+          xlinkHref="#g"
+          id="j"
+          cx="33.867"
+          cy="33.867"
+          r="33.867"
+          fx="33.867"
+          fy="33.867"
+          gradientTransform="matrix(1.13016 0 0 1.18261 -4.408 -6.185)"
+          gradientUnits="userSpaceOnUse"
+        ></radialGradient>
+      </defs>
+      <g paintOrder="fill markers stroke">
+        <path fill="url(#j)" d="M0 0h67.733v67.733H0z"></path>
+        <path fill="#009e00" d="M25.925 0h15.88v67.733h-15.88z"></path>
+        <rect
+          width="15.88"
+          height="67.733"
+          x="25.925"
+          fill="url(#k)"
+          ry="0"
+        ></rect>
+        <path fill="#009e00" d="M67.732 25.927v15.88H-.001v-15.88z"></path>
+        <rect
+          width="15.88"
+          height="67.733"
+          x="25.927"
+          y="-67.732"
+          fill="url(#l)"
+          ry="0"
+          transform="rotate(90)"
+        ></rect>
+        <g transform="translate(116.435 5.25)">
+          <path
+            fill="#be2020"
+            stroke="#faa"
+            strokeWidth="1.125"
+            d="M-89.576 21.246a3.19 3.19 0 0 0-2.994 1.877l-11.158 20.754 1.59 4.233 4.57-1.806 1.713 4.658 4.233-1.59 9.078-23.005 9.078 23.006 4.233 1.59 1.601-5 4.683 2.147 1.59-4.233-11.16-20.754a3.19 3.19 0 0 0-4.232-1.59l-5.793 2.63-5.793-2.63a3.2 3.2 0 0 0-1.239-.287z"
+          ></path>
+          <path
+            fill="#be2020"
+            stroke="#faa"
+            strokeLinejoin="bevel"
+            strokeWidth="0.794"
+            d="M-110.88 11.102c8.944-1.7 17.889 2.725 26.834 11.6l.003 3.287c-7.983 1.644-23.33 8.546-30.726-.375.218-4.837 1.531-9.675 3.888-14.512z"
+          ></path>
+          <path
+            fill="#500"
+            d="M-84.398 25.616c-5.155 2.238-25.174 8.108-30.08-.066-1.93-6.523 16.326 2.063 30.08.066"
+          ></path>
+          <path
+            fill="#be2020"
+            stroke="#faa"
+            strokeLinejoin="bevel"
+            strokeWidth="0.794"
+            d="M-54.255 11.102c-8.945-1.7-17.89 2.725-26.835 11.6l-.003 3.287c7.983 1.644 23.33 8.546 30.726-.375-.219-4.837-1.531-9.675-3.888-14.512z"
+          ></path>
+          <path
+            fill="#500"
+            d="M-80.738 25.616c5.155 2.238 25.174 8.108 30.08-.066 1.93-6.523-16.326 2.063-30.08.066"
+          ></path>
+          <rect
+            width="3.544"
+            height="4.677"
+            x="-84.356"
+            y="21.695"
+            fill="#be2020"
+            stroke="#500"
+            strokeLinejoin="bevel"
+            strokeWidth="0.529"
+            ry="1.276"
+          ></rect>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export function Side() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="100%"
+      height="100%"
+      viewBox="0 0 67.733 67.733"
+    >
+      <defs>
+        <pattern
+          xlinkHref="#b"
+          id="a"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="b"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <pattern
+          xlinkHref="#c"
+          id="i"
+          patternTransform="rotate(45 -276.75 114.634)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="c"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <pattern
+          xlinkHref="#f"
+          id="e"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="f"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <linearGradient id="g">
+          <stop offset="0" stopColor="#970000"></stop>
+          <stop offset="1" stopColor="#f55"></stop>
+        </linearGradient>
+        <linearGradient id="d">
+          <stop offset="0" stopColor="#f55"></stop>
+          <stop offset="1" stopColor="#970000"></stop>
+        </linearGradient>
+        <linearGradient
+          xlinkHref="#d"
+          id="h"
+          x1="33.869"
+          x2="33.869"
+          y1="0"
+          y2="67.733"
+          gradientTransform="translate(0 229.267)"
+          gradientUnits="userSpaceOnUse"
+        ></linearGradient>
+      </defs>
+      <g paintOrder="fill markers stroke" transform="translate(0 -229.267)">
+        <path fill="url(#h)" d="M0 229.267h67.733V297H0z"></path>
+        <path fill="#009e00" d="M25.925 229.267h15.88V297h-15.88z"></path>
+        <rect
+          width="15.88"
+          height="67.733"
+          x="25.925"
+          y="229.267"
+          fill="url(#i)"
+          ry="0"
+        ></rect>
+      </g>
+    </svg>
+  );
+}
+
+export function Bottom() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="100%"
+      height="100%"
+      viewBox="0 0 67.733 67.733"
+    >
+      <defs>
+        <pattern
+          xlinkHref="#a"
+          id="n"
+          patternTransform="rotate(45 81.758 -33.866)scale(4.4039)"
+        ></pattern>
+        <pattern
+          xlinkHref="#b"
+          id="a"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="b"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <pattern
+          xlinkHref="#c"
+          id="i"
+          patternTransform="rotate(45 -276.75 114.634)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="c"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <pattern
+          xlinkHref="#f"
+          id="e"
+          patternTransform="rotate(45)scale(4.4039)"
+        ></pattern>
+        <pattern
+          id="f"
+          width="2"
+          height="2"
+          patternTransform="scale(10)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path fill="#fff" d="M0 0h1v1H0zm1 1h1v1H1z"></path>
+        </pattern>
+        <linearGradient id="g">
+          <stop offset="0" stopColor="#970000"></stop>
+          <stop offset="1" stopColor="#f55"></stop>
+        </linearGradient>
+        <linearGradient id="d">
+          <stop offset="0" stopColor="#f55"></stop>
+          <stop offset="1" stopColor="#970000"></stop>
+        </linearGradient>
+        <linearGradient
+          xlinkHref="#d"
+          id="h"
+          x1="33.869"
+          x2="33.869"
+          y1="0"
+          y2="67.733"
+          gradientTransform="translate(0 229.267)"
+          gradientUnits="userSpaceOnUse"
+        ></linearGradient>
+        <radialGradient
+          xlinkHref="#g"
+          id="m"
+          cx="33.867"
+          cy="33.867"
+          r="33.867"
+          fx="33.867"
+          fy="33.867"
+          gradientTransform="matrix(2.0062 0 0 1.876 -34.077 -29.667)"
+          gradientUnits="userSpaceOnUse"
+        ></radialGradient>
+      </defs>
+      <g paintOrder="fill markers stroke" transform="translate(0 -229.267)">
+        <path fill="url(#h)" d="M0 229.267h67.733V297H0z"></path>
+        <path fill="#009e00" d="M25.925 229.267h15.88V297h-15.88z"></path>
+        <rect
+          width="15.88"
+          height="67.733"
+          x="25.925"
+          y="229.267"
+          fill="url(#i)"
+          ry="0"
+        ></rect>
+      </g>
+      <g paintOrder="fill markers stroke">
+        <path fill="url(#m)" d="M0 0h67.733v67.733H0z"></path>
+        <path fill="#009e00" d="M25.925 0h15.88v67.733h-15.88z"></path>
+        <rect
+          width="15.88"
+          height="67.733"
+          x="25.925"
+          fill="url(#a)"
+          ry="0"
+        ></rect>
+        <path fill="#009e00" d="M67.732 25.927v15.88H-.001v-15.88z"></path>
+        <rect
+          width="15.88"
+          height="67.733"
+          x="25.927"
+          y="-67.732"
+          fill="url(#n)"
+          ry="0"
+          transform="rotate(90)"
+        ></rect>
+      </g>
+    </svg>
+  );
+}
