1bcdb45f581844e32c93bf7c9f833cae7d3194dd
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-17 18:06:17 -0500

    Add idle timeout on mousedown

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Gift from "./components/gift/Gift";
 
 function App() {
-  const [isIdle, setIsIdle] = useState(true);
+  const IDLE_TIMEOUT = 7000;
 
-  const handleClick = () => {
-    setIsIdle((prev) => !prev);
+  const [isIdle, setIsIdle] = useState(true);
+  const timerRef = useRef<number | null>(null);
+
+  const handleMouseDown = () => {
+    setIsIdle(false);
+
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+
+    timerRef.current = setTimeout(() => {
+      setIsIdle(true);
+    }, IDLE_TIMEOUT);
   };
 
   return (
     <main
-      onClick={handleClick}
+      onMouseDown={handleMouseDown}
       className="bg-slate-500/50 h-screen w-screen flex items-center justify-center"
     >
       <Gift isIdle={isIdle} />
