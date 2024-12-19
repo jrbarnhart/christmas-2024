ef9428e253e3e92c137a08f3b3554806d9c48bac
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-19 06:00:28 -0500

    Update style to match music toggle

--- src/components/openGiftButton/OpenGiftButton.tsx
+++ src/components/openGiftButton/OpenGiftButton.tsx
@@ -5,7 +5,7 @@ export default function OpenGiftButton({ appState }: { appState: AppState }) {
     <button
       type="button"
       id="open-button"
-      className="bg-zinc-900 hover:bg-zinc-700 active:bg-zinc-800 text-neutral-50 border-2 border-green-500 w-min mb-10 self-center text-nowrap p-3 rounded-md"
+      className="bg-red-500 hover:bg-red-400 active:bg-red-600 text-neutral-50 border-4 border-green-500 w-min mb-10 self-center text-nowrap p-3 rounded-md"
       onClick={() => {
         appState.isBoxOpen.set((val) => !val);
         appState.isDragging.set(false);
