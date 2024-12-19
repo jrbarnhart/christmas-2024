79a6f4c48c24da3c0cae8b7722e09c26c777ac6a
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-19 05:26:56 -0500

    Add music toggle to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Gift from "./components/gift/Gift";
+import MusicToggle from "./components/music/MusicToggle";
 import OpenGiftButton from "./components/openGiftButton/OpenGiftButton";
 import Snowflakes from "./components/snowflakes/Snowflakes";
 import useAppState from "./hooks/useAppState";
 import useControls from "./hooks/useControls";
+import useMusic from "./hooks/useMusic";
 
 function App() {
   const appState = useAppState();
   const { handleMouseDown, cubeRef } = useControls({ appState });
+  const music = useMusic();
 
   return (
     <main className="bg-gradient-to-tr from-red-300 to-green-300 via-white h-screen w-screen touch-none grid grid-rows-3 grid-cols-3">
@@ -22,6 +25,9 @@ function App() {
       </div>
       <div className="row-span-full col-span-full pointer-events-none touch-none">
         <Snowflakes />
+      </div>
+      <div className="row-start-1 col-start-3 flex justify-end mr-20 mt-20">
+        <MusicToggle appState={appState} music={music} />
       </div>
     </main>
   );
