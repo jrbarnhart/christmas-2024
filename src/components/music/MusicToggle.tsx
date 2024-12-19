9df76a9cc8c2e6648631563c885fe50d2da9ef83
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-19 05:47:05 -0500

    Add icons from lucide based on state of music

--- src/components/music/MusicToggle.tsx
+++ src/components/music/MusicToggle.tsx
@@ -1,12 +1,13 @@
 import { useCallback } from "react";
 import { AppState } from "../../hooks/useAppState";
-import { Music } from "../../hooks/useMusic";
+import { UseMusicType } from "../../hooks/useMusic";
+import { Music, VolumeOff } from "lucide-react";
 
 export default function MusicToggle({
   music,
   appState,
 }: {
-  music: Music;
+  music: UseMusicType;
   appState: AppState;
 }) {
   const handleClick = useCallback(() => {
@@ -14,16 +15,23 @@ export default function MusicToggle({
       music.current.pause();
       appState.isMusicPlaying.set(false);
     } else {
-      music.current.play().catch(() => {
-        console.error("There was an error while starting the music.");
-      });
+      music.current
+        .play()
+        .then(() => {
+          appState.isMusicPlaying.set(true);
+        })
+        .catch(() => {
+          console.error("There was an error while starting the music.");
+        });
     }
   }, [appState.isMusicPlaying, music]);
   return (
     <button
       onClick={handleClick}
       type="button"
-      className="h-10 w-10 bg-red-500 rounded-full"
-    ></button>
+      className="h-10 w-10 bg-red-500 rounded-full flex items-center justify-center"
+    >
+      {appState.isMusicPlaying.value ? <VolumeOff /> : <Music />}
+    </button>
   );
 }
