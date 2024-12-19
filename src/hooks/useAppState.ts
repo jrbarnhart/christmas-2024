3dc0f3337d0bc9f5727d44a72cf7d88855e189bb
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-19 00:18:31 -0500

    Add hook that returns container with app state

---
+++ src/hooks/useAppState.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function useAppState() {
+  const [currentRotation, setCurrentRotation] = useState({ x: -30, y: -45 });
+  const [momentum, setMomentum] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+
+  const appState = {
+    currentRotation: {
+      value: currentRotation,
+      set: setCurrentRotation,
+    },
+    momentum: {
+      value: momentum,
+      set: setMomentum,
+    },
+    isDragging: {
+      value: isDragging,
+      set: setIsDragging,
+    },
+  };
+
+  return appState;
+}
+
+export type AppState = ReturnType<typeof useAppState>;
