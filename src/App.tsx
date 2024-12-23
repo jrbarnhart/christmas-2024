247d07767c80086ae6c81134617498a4b31ddd0f
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-23 18:53:14 -0500

    Disable music with comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Gift from "./components/gift/Gift";
 import Message from "./components/message/Message";
-import MusicToggle from "./components/music/MusicToggle";
+// import MusicToggle from "./components/music/MusicToggle";
 import OpenGiftButton from "./components/openGiftButton/OpenGiftButton";
 import Snowflakes from "./components/snowflakes/Snowflakes";
 import useAppState from "./hooks/useAppState";
 import useControls from "./hooks/useControls";
-import useMusic from "./hooks/useMusic";
+// import useMusic from "./hooks/useMusic";
 
 function App() {
   const appState = useAppState();
   const { handleMouseDown, cubeRef } = useControls({ appState });
-  const music = useMusic({ appState });
+  // const music = useMusic({ appState });
 
   return (
     <main className="bg-gradient-to-tr from-red-300 to-green-300 via-white h-screen w-screen touch-none grid grid-rows-3 grid-cols-3">
@@ -32,9 +32,9 @@ function App() {
       <div className="row-span-full col-span-full pointer-events-none touch-none">
         <Snowflakes />
       </div>
-      <div className="row-start-1 row-end-1 col-start-1 col-end-1 flex justify-start p-5 md:p-16 h-min w-min">
+      {/*       <div className="row-start-1 row-end-1 col-start-1 col-end-1 flex justify-start p-5 md:p-16 h-min w-min">
         <MusicToggle appState={appState} music={music} />
-      </div>
+      </div> */}
     </main>
   );
 }
